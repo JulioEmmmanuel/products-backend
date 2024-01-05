38e0cec8c1982702bddc2dd76868ab540c400c1c
julio2505 <juliomeza2510@outlook.com>
2024-01-05 00:40:16 -0600

    deploy

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const routerApi = require("./routes");
 const { logErrors, errorHandler, boomErrorHandler } = require("./middlewares/error.handler")
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -20,15 +20,9 @@ const options = {
 }
 app.use(cors(options))
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Hola mi server en express");
 });
-
-app.get("/nueva-ruta", (req, res) => {
-  res.send("Hola soy una nueva ruta")
-})
-
-
 
 routerApi(app);
 
